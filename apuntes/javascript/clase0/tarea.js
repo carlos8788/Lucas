ac7957426ef328d34d9e18b2900212e691bc0408
carlos8788 <carlos8788@gmail.com>
2023-09-16 13:00:50 -0300

    tarea clase 0

---
+++ apuntes/javascript/clase0/tarea.js
@@ -0,0 +1,5 @@
+// MOSTRAR EN EL HTML
+// Colocar en un h2 el nombre completo y la edad
+// crear un elemento p que tenga el texto (lorem ipsun lorem ipsun lorem ipsun) introducido por javascript 
+
+ // MOSTRAR EN LA CONSOLA el texto 'es la clase cero de JS'
