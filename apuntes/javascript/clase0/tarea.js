994bca82f618196be6fe98aeb5a7f337337f2ce8
Lucas <fer-gonzalez94@hotmail.com>
2023-10-13 12:00:29 -0300

    Tarea 1 terminada

--- apuntes/javascript/clase0/tarea.js
+++ apuntes/javascript/clase0/tarea.js
@@ -1,5 +1,32 @@
-// MOSTRAR EN EL HTML
-// Colocar en un h2 el nombre completo y la edad
-// crear un elemento p que tenga el texto (lorem ipsun lorem ipsun lorem ipsun) introducido por javascript 
+// Crear una constante cualquiera y mostrarla por un console.log
+const COSA = "COMPUTADORA"
+console.log(COSA)
 
- // MOSTRAR EN LA CONSOLA el texto 'es la clase cero de JS'
+// Pedir por pront "Nombre y apellido " y luego mostrar una oracion mostrando los mismos
+
+// let dato1 = prompt('Ingrese Nombre')
+// let dato2 = prompt('Ingrese Apellido')
+
+// const NOMBRE = "NOMBRE"
+// const APELLIDO = "APELLIDO"
+
+const NOMBRE = prompt("NOMBRE")
+const APELLIDO = prompt("APELLIDO")
+
+let oracionCorregida = `${NOMBRE}, ${APELLIDO}`
+console.log(oracionCorregida)
+
+
+// Pedir por pront "Edad" y devolver posible año de nacimiento 
+
+//1º definir una constante edad 
+const EDAD = prompt("EDAD")
+
+//2º definir una variable que obtenga el año de nacimiento
+const ANIO_ACTUAL = 2023
+
+let resta = ANIO_ACTUAL - Number(EDAD);
+
+//3º mostrar en consola
+
+console.log(resta)
