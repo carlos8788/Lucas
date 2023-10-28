3e4eb122dc326d6c27123febc9ce3c986bf1f552
Lucas <fer-gonzalez94@hotmail.com>
2023-10-28 14:21:45 -0300

    mensaje

--- apuntes/javascript/clase_2/tarea.js
+++ apuntes/javascript/clase_2/tarea.js
@@ -1,3 +1,53 @@
 // Pedir número mediante prompt y si es mayor a 1000 mostrar un alert.
+
+
+
+let numero = prompt("Por favor, ingresa un número:");
+
+if (numero < 1000) {
+      console.log("Es menor a 1000")
+alert("es mayor a 1000.");
+    }
+    else{
+        console.log('Es mayor a 1000')
+alert("Es mayor a 1000.");
+    }
+    
+
+
 // Pedir un texto mediante prompt, y si es igual a "Hola" mostrar un alerta por consola.
+
+// const SALUDO = 'hola'
+// const EDUCADO = prompt('Ingrese su EDUCADO')
+
+// if(EDUCADO === SALUDO){
+//     console.log('Ud es educado')
+// alert("Usted saluda.");
+// }
+// else{
+//     console.log('Ud no es mal educado')
+// alert("Usted no saludo.");
+// }
+
+
+
+
+
+
 // Pedir un número por prompt y evaluar si está entre 10 y 50. En caso positivo mostrar un alert.
+
+
+// let numero = Number(prompt('Valor del Ingrese uun numero  entre 10 y 50'))
+
+// if (numero < 10) {
+//     console.log("Es menor a 10")
+//     alert("El número es menor a 10.");
+// }
+// else if (numero >= 10 && numero <= 50) {
+//     console.log("Está en el rango")
+//     alert("El número ingresado está entre 10 y 50 (incluyendo 10 y 50).");
+// }
+// else {
+//     console.log("Es mayor a 50")
+//     alert("El número es mayor a 50.");
+// }
