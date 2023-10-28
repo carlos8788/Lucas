20c159d5238591fb6f985a6711b75278b1c19ee5
carlos8788 <carlos8788@gmail.com>
2023-10-28 17:31:16 -0300

    pokemones

--- apuntes/javascript/clase_5/pokemon.js
+++ apuntes/javascript/clase_5/pokemon.js
@@ -278,15 +278,341 @@ let pokemones = [
     {
         name: "weepinbell",
         url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/70.png"
+    },
+    {
+        name: "victreebel",
+        url: "https://pokeapi.co/api/v2/pokemon/71/"
+    },
+    {
+        name: "tentacool",
+        url: "https://pokeapi.co/api/v2/pokemon/72/"
+    },
+    {
+        name: "tentacruel",
+        url: "https://pokeapi.co/api/v2/pokemon/73/"
+    },
+    {
+        name: "geodude",
+        url: "https://pokeapi.co/api/v2/pokemon/74/"
+    },
+    {
+        name: "graveler",
+        url: "https://pokeapi.co/api/v2/pokemon/75/"
+    },
+    {
+        name: "golem",
+        url: "https://pokeapi.co/api/v2/pokemon/76/"
+    },
+    {
+        name: "ponyta",
+        url: "https://pokeapi.co/api/v2/pokemon/77/"
+    },
+    {
+        name: "rapidash",
+        url: "https://pokeapi.co/api/v2/pokemon/78/"
+    },
+    {
+        name: "slowpoke",
+        url: "https://pokeapi.co/api/v2/pokemon/79/"
+    },
+    {
+        name: "slowbro",
+        url: "https://pokeapi.co/api/v2/pokemon/80/"
+    },
+    {
+        name: "magnemite",
+        url: "https://pokeapi.co/api/v2/pokemon/81/"
+    },
+    {
+        name: "magneton",
+        url: "https://pokeapi.co/api/v2/pokemon/82/"
+    },
+    {
+        name: "farfetchd",
+        url: "https://pokeapi.co/api/v2/pokemon/83/"
+    },
+    {
+        name: "doduo",
+        url: "https://pokeapi.co/api/v2/pokemon/84/"
+    },
+    {
+        name: "dodrio",
+        url: "https://pokeapi.co/api/v2/pokemon/85/"
+    },
+    {
+        name: "seel",
+        url: "https://pokeapi.co/api/v2/pokemon/86/"
+    },
+    {
+        name: "dewgong",
+        url: "https://pokeapi.co/api/v2/pokemon/87/"
+    },
+    {
+        name: "grimer",
+        url: "https://pokeapi.co/api/v2/pokemon/88/"
+    },
+    {
+        name: "muk",
+        url: "https://pokeapi.co/api/v2/pokemon/89/"
+    },
+    {
+        name: "shellder",
+        url: "https://pokeapi.co/api/v2/pokemon/90/"
+    },
+    {
+        name: "cloyster",
+        url: "https://pokeapi.co/api/v2/pokemon/91/"
+    },
+    {
+        name: "gastly",
+        url: "https://pokeapi.co/api/v2/pokemon/92/"
+    },
+    {
+        name: "haunter",
+        url: "https://pokeapi.co/api/v2/pokemon/93/"
+    },
+    {
+        name: "gengar",
+        url: "https://pokeapi.co/api/v2/pokemon/94/"
+    },
+    {
+        name: "onix",
+        url: "https://pokeapi.co/api/v2/pokemon/95/"
+    },
+    {
+        name: "drowzee",
+        url: "https://pokeapi.co/api/v2/pokemon/96/"
+    },
+    {
+        name: "hypno",
+        url: "https://pokeapi.co/api/v2/pokemon/97/"
+    },
+    {
+        name: "krabby",
+        url: "https://pokeapi.co/api/v2/pokemon/98/"
+    },
+    {
+        name: "kingler",
+        url: "https://pokeapi.co/api/v2/pokemon/99/"
+    },
+    {
+        name: "voltorb",
+        url: "https://pokeapi.co/api/v2/pokemon/100/"
+    },
+    {
+        name: "electrode",
+        url: "https://pokeapi.co/api/v2/pokemon/101/"
+    },
+    {
+        name: "exeggcute",
+        url: "https://pokeapi.co/api/v2/pokemon/102/"
+    },
+    {
+        name: "exeggutor",
+        url: "https://pokeapi.co/api/v2/pokemon/103/"
+    },
+    {
+        name: "cubone",
+        url: "https://pokeapi.co/api/v2/pokemon/104/"
+    },
+    {
+        name: "marowak",
+        url: "https://pokeapi.co/api/v2/pokemon/105/"
+    },
+    {
+        name: "hitmonlee",
+        url: "https://pokeapi.co/api/v2/pokemon/106/"
+    },
+    {
+        name: "hitmonchan",
+        url: "https://pokeapi.co/api/v2/pokemon/107/"
+    },
+    {
+        name: "lickitung",
+        url: "https://pokeapi.co/api/v2/pokemon/108/"
+    },
+    {
+        name: "koffing",
+        url: "https://pokeapi.co/api/v2/pokemon/109/"
+    },
+    {
+        name: "weezing",
+        url: "https://pokeapi.co/api/v2/pokemon/110/"
+    },
+    {
+        name: "rhyhorn",
+        url: "https://pokeapi.co/api/v2/pokemon/111/"
+    },
+    {
+        name: "rhydon",
+        url: "https://pokeapi.co/api/v2/pokemon/112/"
+    },
+    {
+        name: "chansey",
+        url: "https://pokeapi.co/api/v2/pokemon/113/"
+    },
+    {
+        name: "tangela",
+        url: "https://pokeapi.co/api/v2/pokemon/114/"
+    },
+    {
+        name: "kangaskhan",
+        url: "https://pokeapi.co/api/v2/pokemon/115/"
+    },
+    {
+        name: "horsea",
+        url: "https://pokeapi.co/api/v2/pokemon/116/"
+    },
+    {
+        name: "seadra",
+        url: "https://pokeapi.co/api/v2/pokemon/117/"
+    },
+    {
+        name: "goldeen",
+        url: "https://pokeapi.co/api/v2/pokemon/118/"
+    },
+    {
+        name: "seaking",
+        url: "https://pokeapi.co/api/v2/pokemon/119/"
+    },
+    {
+        name: "staryu",
+        url: "https://pokeapi.co/api/v2/pokemon/120/"
+    },
+    {
+        name: "starmie",
+        url: "https://pokeapi.co/api/v2/pokemon/121/"
+    },
+    {
+        name: "mr-mime",
+        url: "https://pokeapi.co/api/v2/pokemon/122/"
+    },
+    {
+        name: "scyther",
+        url: "https://pokeapi.co/api/v2/pokemon/123/"
+    },
+    {
+        name: "jynx",
+        url: "https://pokeapi.co/api/v2/pokemon/124/"
+    },
+    {
+        name: "electabuzz",
+        url: "https://pokeapi.co/api/v2/pokemon/125/"
+    },
+    {
+        name: "magmar",
+        url: "https://pokeapi.co/api/v2/pokemon/126/"
+    },
+    {
+        name: "pinsir",
+        url: "https://pokeapi.co/api/v2/pokemon/127/"
+    },
+    {
+        name: "tauros",
+        url: "https://pokeapi.co/api/v2/pokemon/128/"
+    },
+    {
+        name: "magikarp",
+        url: "https://pokeapi.co/api/v2/pokemon/129/"
+    },
+    {
+        name: "gyarados",
+        url: "https://pokeapi.co/api/v2/pokemon/130/"
+    },
+    {
+        name: "lapras",
+        url: "https://pokeapi.co/api/v2/pokemon/131/"
+    },
+    {
+        name: "ditto",
+        url: "https://pokeapi.co/api/v2/pokemon/132/"
+    },
+    {
+        name: "eevee",
+        url: "https://pokeapi.co/api/v2/pokemon/133/"
+    },
+    {
+        name: "vaporeon",
+        url: "https://pokeapi.co/api/v2/pokemon/134/"
+    },
+    {
+        name: "jolteon",
+        url: "https://pokeapi.co/api/v2/pokemon/135/"
+    },
+    {
+        name: "flareon",
+        url: "https://pokeapi.co/api/v2/pokemon/136/"
+    },
+    {
+        name: "porygon",
+        url: "https://pokeapi.co/api/v2/pokemon/137/"
+    },
+    {
+        name: "omanyte",
+        url: "https://pokeapi.co/api/v2/pokemon/138/"
+    },
+    {
+        name: "omastar",
+        url: "https://pokeapi.co/api/v2/pokemon/139/"
+    },
+    {
+        name: "kabuto",
+        url: "https://pokeapi.co/api/v2/pokemon/140/"
+    },
+    {
+        name: "kabutops",
+        url: "https://pokeapi.co/api/v2/pokemon/141/"
+    },
+    {
+        name: "aerodactyl",
+        url: "https://pokeapi.co/api/v2/pokemon/142/"
+    },
+    {
+        name: "snorlax",
+        url: "https://pokeapi.co/api/v2/pokemon/143/"
+    },
+    {
+        name: "articuno",
+        url: "https://pokeapi.co/api/v2/pokemon/144/"
+    },
+    {
+        name: "zapdos",
+        url: "https://pokeapi.co/api/v2/pokemon/145/"
+    },
+    {
+        name: "moltres",
+        url: "https://pokeapi.co/api/v2/pokemon/146/"
+    },
+    {
+        name: "dratini",
+        url: "https://pokeapi.co/api/v2/pokemon/147/"
+    },
+    {
+        name: "dragonair",
+        url: "https://pokeapi.co/api/v2/pokemon/148/"
+    },
+    {
+        name: "dragonite",
+        url: "https://pokeapi.co/api/v2/pokemon/149/"
+    },
+    {
+        name: "mewtwo",
+        url: "https://pokeapi.co/api/v2/pokemon/150/"
+    },
+    {
+        name: "mew",
+        url: "https://pokeapi.co/api/v2/pokemon/151/"
     }
 ]
 
+const pokeresult = pokemones.map((pokemon, key) => pokemon.url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${key+1}.png`)
+console.log(pokeresult[0]);
 // Crear Variable
 
 let pokeFront = ''
 
 // rellenar variable
-pokemones.forEach(
+pokeresult.forEach(
     function (poke) {
         pokeFront += `<div class="card-pokemon">
                         <h4>${poke.name}</h4>
