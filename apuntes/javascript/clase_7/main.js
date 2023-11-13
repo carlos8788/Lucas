1acce171f36ce989fef399832156ea7db5149e65
Lucas <fer-gonzalez94@hotmail.com>
2023-11-12 21:13:36 -0300

    Pushiado

--- apuntes/javascript/clase_7/main.js
+++ apuntes/javascript/clase_7/main.js
@@ -1,12 +1,24 @@
-const array = [];
-console.log(array)
+// const array = [];
+// console.log(array)
 
-array.push('Luis')
-console.log(array)
+// array.push('Luis')
+// console.log(array)
 
-array.push('Lucas')
-console.log(array)
+const perro = {
+    nombre: "draco",
+    raza : "nike",
+    edad:"2 meses",
+    color : ["negro","blanco","marron"]
+}
 
-array = 10
-console.log(array)
-
+function crearElemento(paco){
+    const contenedor = document.querySelector('#contenedor-personas')
+    const elemento = document.createElement('h3')
+    elemento.textContent = `nombre:${paco.nombre} 
+     raza:${paco.raza}
+     edad:${paco.edad} 
+     color:${paco.color[1]}, ${paco.color[0]}, ${paco.color[2]}`
+    contenedor.append(elemento)
+}
+crearElemento(perro)
+console.log(perro.raza)
