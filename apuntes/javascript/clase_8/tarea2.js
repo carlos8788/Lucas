3ab7c6dea72bc9d46348f72089133ce0ff3024d9
Lucas <fer-gonzalez94@hotmail.com>
2023-11-24 22:43:06 -0300

    TAREA HECHA

--- apuntes/javascript/clase_8/tarea2.js
+++ apuntes/javascript/clase_8/tarea2.js
@@ -1,5 +1,5 @@
 let frutas = [
-    { nombre: "Manzana", precio: 0.5, cantidad: 10 },
+    { nombre: "Manzana", precio: -0.6, cantidad: 10 },
     { nombre: "Banana", precio: 0.3, cantidad: 5 },
     { nombre: "Naranja", precio: 0.7, cantidad: 8 },
     { nombre: "Mango", precio: 1.5, cantidad: 2 },
@@ -15,37 +15,44 @@ let frutas = [
 
 
 
-function actualizarPrecio(arrayFrutas, indice, precioNuevo) {
-    arrayFrutas[indice].precio = precioNuevo
+// function actualizarPrecio(arrayFrutas, indice, precioNuevo) {
+//     arrayFrutas[indice].precio = precioNuevo
 
-}
-
-
-
-actualizarPrecio(frutas, 4, 600)
-actualizarPrecio(frutas, 0, 350)
+// }
 
 
-function actualizarCanti(arrayFrutas, indice, cantidadNueva) {
-    if (arrayFrutas.length - 1 <=indice){
-        if(cantidadNueva > 0){
-            arrayFrutas[indice].cantidad = cantidadNueva
-        }
-        else{
-            console.log('La cantidad no puede ser menor a 0');
-        }
-    }else{
-        console.log('El indice no es correcto');
-    }
-    
 
-}
+// actualizarPrecio(frutas, 4, 600)
+// actualizarPrecio(frutas, 0, 350)
 
-actualizarCanti(frutas, 0, 90)
-actualizarCanti(frutas, 4, -20)
 
-console.log(frutas.length);
-console.log(frutas.length - 1);
+// function actualizarCanti(arrayFrutas, indice, cantidadNueva) {    if (arrayFrutas.length - 1 <=indice){
+//         if(cantidadNueva > 0){
+//             arrayFrutas[indice].cantidad = cantidadNueva
+//         }
+//         else{
+//             console.log('La cantidad no puede ser menor a 0');
+//         }
+//     }else{
+//         console.log('El indice no es correcto');
+//     }
+
+
+// }
+
+// actualizarCanti(frutas, 0, 90)
+// actualizarCanti(frutas, 4, -20)
+
+// console.log(frutas.length);
+// console.log(frutas.length - 1);
 
 // TAREA
 // Condicionar para que el precio sea mayor a 0, y que el indice también no sea mayor al indice del ultimo elemento
+
+for (let i = 0; i < frutas.length; i++) {
+    // Condición: Precio mayor que 0 y el índice no mayor que el índice del último elemento
+    if (frutas[i].precio > 0 && i <= frutas.length - 1)
+     {
+        console.log(`Nombre: ${frutas[i].nombre}, Precio: ${frutas[i].precio}, Cantidad: ${frutas[i].cantidad}`);
+    }
+}
